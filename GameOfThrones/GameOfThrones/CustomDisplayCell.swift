e58d8ea09aeff6e2654c25f335491909b30dedf6
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2018-11-18 13:03:10 -0500

    first setUp

---
+++ GameOfThrones/GameOfThrones/CustomDisplayCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomDisplayCell.swift
+//  GameOfThrones
+//
+//  Created by Elizabeth Peraza  on 11/18/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class CustomDisplayCell: UITableViewCell {
+
+    @IBOutlet weak var gOTImage: UIImageView!
+    @IBOutlet weak var episodeName: UILabel!
+    @IBOutlet weak var seasonEpisode: UILabel!
+    
+    
+    
+    
+
+}
--- GameOfThrones/GameOfThrones/ViewController.swift
+++ GameOfThrones/GameOfThrones/ViewController.swift
@@ -9,15 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    private var allEpisodes = GOTEpisode.allEpisodes
+    
+    
   override func viewDidLoad() {
+    
     super.viewDidLoad()
     print("test test test")
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
+    tableView.delegate = self
+    tableView.dataSource = self
+    
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let indexPath = tableView.indexPathForSelectedRow,
+            let detailedGOTController = segue.destination as? DetailedGOTController else {return}
+        
+        let currentEpisode = allEpisodes[indexPath.row]
+        detailedGOTController.episode = currentEpisode
+    }
     
     
+}
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allEpisodes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GOTCell", for: indexPath) as? CustomDisplayCell else {fatalError("episode not found")}
+        
+        let currentEpisode = allEpisodes[indexPath.row]
+        cell.gOTImage.image = UIImage(named: currentEpisode.mediumImageID)
+        cell.episodeName.text = currentEpisode.name
+        cell.seasonEpisode.text = "S: \(currentEpisode.season). Episode: \(currentEpisode.number)"
+        
+        return cell
+    }
+    
+
+}
+
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
 
