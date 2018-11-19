79175001dc2baf5c9c9e72a4cb5adaaa094f5bf4
JoshuaViera <joshuaviera95@gmail.com>
2018-11-19 16:53:51 -0500

    second commit

---
+++ GameOfThrones/GameOfThrones/Views/GOTviewCell.swift
@@ -0,0 +1,28 @@
+//
+//  GOTviewCell.swift
+//  GameOfThrones
+//
+//  Created by Joshua Viera on 11/19/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class GOTviewCell: UITableViewCell {
+
+    @IBOutlet weak var GOTimage: UIImageView!
+    @IBOutlet weak var seasonAndEpisode: UILabel!
+    @IBOutlet weak var episodeName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
