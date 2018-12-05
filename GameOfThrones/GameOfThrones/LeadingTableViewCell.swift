b198402932f720d6444d46eb7d09bd6edf436c95
Ramu <ramu@Pursuits-MacBook-Pro-4.local>
2018-12-05 17:15:46 -0500

    completed assignment

---
+++ GameOfThrones/GameOfThrones/LeadingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeadingTableViewCell.swift
+//  GameOfThrones
+//
+//  Created by Ramu on 12/5/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class LeadingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
