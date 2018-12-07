936dc6bbc9c54f60ccd8a6acf46c69d6277f5e65
tawnyblvd <arielrivera@pursuit.org>
2018-12-07 11:40:54 -0500

    detail view labels

--- GameOfThrones/GameOfThrones/Views/EpisodeViewController.swift
+++ GameOfThrones/GameOfThrones/Views/EpisodeViewController.swift
@@ -11,12 +11,26 @@ import UIKit
 class EpisodeViewController: UIViewController {
     
     var gotEpisodes: GOTEpisode!
+    
+    @IBOutlet weak var seasonLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var runtimeLabel: UILabel!
+    
+    @IBOutlet weak var airdateLabel: UILabel!
+    
 
     @IBOutlet weak var episodeTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        seasonLabel.text = "Season: " + gotEpisodes.season.description
+        nameLabel.text = "Episode: " + gotEpisodes.name
+        runtimeLabel.text = "Runtime: " + gotEpisodes.runtime.description
+        airdateLabel.text = "Airdate: " + gotEpisodes.airdate
+        
         episodeTextView.text = gotEpisodes.summary
         
     }
