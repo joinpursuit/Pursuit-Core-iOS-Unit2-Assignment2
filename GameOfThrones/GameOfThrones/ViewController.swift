e6e52fb7f31c2c78218fb4708547c50b5c1a4fc5
LubaKaper <liubovkaper@pursuit.com>
2019-11-19 22:35:30 -0500

    tableView with 2 tableView cells

--- GameOfThrones/GameOfThrones/ViewController.swift
+++ GameOfThrones/GameOfThrones/ViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var seasons = [[GOTEpisode]]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    seasons = GOTEpisode.getSeasonSections()
+    tableView.dataSource = self
+    tableView.delegate = self
+    
+    
   }
 
 
 }
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return seasons[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section % 2 == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "firstCell", for: indexPath) as? FirstTableViewCell else {
+                fatalError("couldn't deque FirstTableViewCell")
+            }
+            let episode = seasons[indexPath.section][indexPath.row]
+            cell.configureFirstCell(for: episode)
+            return cell
+        } else  {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "secondCell", for: indexPath) as? SecondTableViewCell else {
+                fatalError("couldn't deque SecondTableViewCell")
+            }
+            let episode = seasons[indexPath.section][indexPath.row]
+            cell.configureSecondCell(for: episode)
+            return cell
+        }
+    }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return seasons.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Season \(seasons[section].first?.season.description ?? "")"
+    }
+}
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+}
