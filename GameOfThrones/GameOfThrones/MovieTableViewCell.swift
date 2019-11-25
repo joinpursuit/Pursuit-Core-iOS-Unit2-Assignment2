5cd3e208d717e882d36884d7c5dcddaef7ef865a
Yuliia Engman <yuliia.engman@gmail.com>
2019-11-25 17:26:20 -0500

    basic code

---
+++ GameOfThrones/GameOfThrones/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  GameOfThrones
+//
+//  Created by Yuliia Engman on 11/25/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var subLabel: UILabel!
+    
+    func configureCell(for movie: GOTEpisode) {
+           movieImage.image = UIImage(named: movie.mediumImageID.description)
+           nameLabel.text = movie.name
+           subLabel.text = String(movie.season)
+       }
+
+}
--- GameOfThrones/GameOfThrones/ViewController.swift
+++ GameOfThrones/GameOfThrones/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movieEpisodes = [[GOTEpisode]]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        loadData()
+    }
+    
+    func loadData() {
+        movieEpisodes = [GOTEpisode.allEpisodes]
+    }
+    
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieEpisodes[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: MovieTableViewCell!
+        
+        if indexPath.section % 2 == 0 {
+            guard let movieCell = tableView.dequeueReusableCell(withIdentifier: "rightImageCell", for: indexPath) as? MovieTableViewCell else {
+                fatalError("We coudn't degueue a rightImageCell")
+            }
+            cell = movieCell
+        } else {
+        
+        let episode = movieEpisodes[indexPath.section][indexPath.row]
+        cell.configureCell(for: episode)
+        
+        return cell
+    }
+}
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return movieEpisodes[section].first?.season
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        movieEpisodes.count
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+}
